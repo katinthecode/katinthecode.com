835cc49251e6c214059550405105ce5bad48fb16
Katie <katinthecode@gmail.com>
2023-12-04 17:33:40 -0600

    Updating contact info and other files

---
+++ js/JavaScriptForReact.js
@@ -0,0 +1,34 @@
+function DoSomething() {
+}
+
+const DoSomething = () => {
+}
+
+let age = 10;
+let name = "Pedro";
+
+if (age > 10) {
+	name = "Pedro"
+} else {
+	name = "Katie"
+}
+
+let name = age > 10 ? "Pedro" : "Jack";
+
+const Component = () =>
+{
+	return <div> Pedro </div>;
+}
+
+let names = ["Pedro", "Jessica", "Carol"];
+const names2 = [...names, "Joel"];
+
+names.map((name) => {
+	return name + "1"
+});
+
+names.filter(() => {
+	return name !== "Pedro"
+});
+
+
